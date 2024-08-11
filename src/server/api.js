865719fb39a65ebb2937a7a0cf645f4544d6c27f
ToanJt <lexuantoan5703@gmail.com>
2024-08-11 19:57:30 +0700

    fix

--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/api/data";
+const API_URL = "https://studentop.onrender.com/api/data";
 
 export const getData = () => {
   return axios.get(API_URL);
