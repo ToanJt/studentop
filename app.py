323b3459d9491055c65ed8d538b14600f0b9f687
ToanJt <lexuantoan5703@gmail.com>
2024-08-11 18:50:57 +0700

    fix

--- app.py
+++ app.py
@@ -25,5 +25,8 @@ def add_data():
     mongo.db.student.insert_one(data)
     return jsonify(message="Data added successfully"), 201
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
